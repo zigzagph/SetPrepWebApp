734f09758cfa673eeed4f85097dfaa9d426f0cf6
Patrick Hutchinson <zigzagph@gmail.com>
2018-10-15 09:11:46 -0500

    More updates, fixed the active button issue, now filtering out unknown songs. Need to get the dropdown value and apply to the api call.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 
 import { Song } from './song';
-//import { concatAll } from 'rxjs/operators';
-//import { ThrowStmt } from '@angular/compiler';
-//import { SongComponent } from './song/song.component';
 
 @Component({
     selector: 'app-root',
@@ -14,21 +11,25 @@ import { Song } from './song';
 export class AppComponent implements OnInit {
 
     songArray: Array<Song> = [];
-    purgeSongs: Array<string> = ['1/30/97 Jam', '12345 678910 11 12', '[interview]'];
+    coversArray: Array<Song> = [];
+    totalCovers = 0;
+    purgeSongs: Array<string> = ['1/30/97 Jam', '12345 678910 11 12', '[interview]', 'Unknown', 'Costume Contest Jam', 'Countdown Medley', 'Disco Bisquick', 'Flip-Flop', 'Fuck That Doll', 'Giveaway', 'Hibbage', 'Ida Fith', 'Jam', 'Jam On The River Song', 'Killing Eleven', 'Koyaanisqatsi Jam', 'Latin Pussy', 'Leora', 'Loch Ness Monster', 'Macina Verde', 'Manic Depression', 'Mariel Bop Bop', 'Marvelous', 'new song', 'Ohayou Gozaimasu', 'Onery Funk', 'Paul Revere', 'Road Song', 'Salute To Sammy', 'Smash', 'Smell The Funk', 'South Of The Border Pussy', 'Sprawl', 'Sugarcane', 'Termites', 'The Great Pumpkin Jam', 'The Truth', 'Tony\'s Birthday Jam', 'Ulua', 'Waltz In Black', 'What A Show', 'Worcester Jam'];
 
     previousThreeArray: Array<Song> = [];
-    previousThreeActive = true;
-    previousThreeColor = 'red';
+    previousThreeColor = '#f10f0f';
+
+    checkModel: any = { left: false, middle: true, right: false };
 
     constructor(private appService: AppService) {}
     
     ngOnInit(): void {
 
+
         // get all of the bands songs
         this.appService.getSongs().subscribe(
             showData => { 
                 console.log("Count : " + showData.length);
-                
+                //console.log(this.color);
                 // Alphabetiallby sort the string array
                 showData.sort(function(a, b){
                     var x = a.Name.toLowerCase();
@@ -54,6 +55,9 @@ export class AppComponent implements OnInit {
                         this.songArray.push(s);
                     }
                 });
+
+                // Create the covers array
+                this.coversArray = this.songArray.filter(song => song.cover);
             },
             error => { console.log(error); }
         );
@@ -83,9 +87,16 @@ export class AppComponent implements OnInit {
         ); */
     }
 
-    getPreviousShows(): void {
+    test(): void {
+        console.log("test");
+    }
+
+    getPreviousThreeShows(): void {
         this.appService.getPrevious().subscribe(
             showData => {
+                // temp array
+                let sArray = [];
+                
                 // for each show
                 showData.forEach(show => {
                     // for each set
@@ -94,36 +105,46 @@ export class AppComponent implements OnInit {
                         set.songs.forEach(song => {
                             let arr = this.songArray.filter(s => s.id === song.id);
                             arr[0].color = this.previousThreeColor;
-                            this.previousThreeArray.push(arr[0]);
-
-                            /* console.log(this.previousThreeArray); */
+                            sArray.push(arr[0]);
                         });
                     });
                 });
+
+                // filter out the duplicates
+                this.previousThreeArray = sArray.filter(function(item, pos) {
+                    return sArray.indexOf(item) == pos;
+                })
+
             },
             error => { console.log(error); }
         );
     }
 
-    colorEvent(color: any): void {
+    setPreviousThreeColorEvent(color: any): void {
         this.previousThreeColor = color;
 
         this.previousThreeArray.forEach(song => {
-            song.color = color;
+            song.color = this.previousThreeColor;
         })
     }
 
     filterPreviousShows(): void {
-        console.log("filtering...");
-        //console.log(this.previousThreeArray);
+        //console.log("filtering...");
 
-        if( this.previousThreeArray.length === 0 ){
-            this.previousThreeActive = !this.previousThreeActive;
-
+        if( this.previousThreeArray.length > 0 ){
             this.previousThreeArray.forEach(song => {
-                //song.hidden = true;
-                if( song.color === this.previousThreeColor ){
-                    song.hidden = true;
+                song.hidden = !song.hidden;
+            })
+        }
+    }
+
+    filterCovers(): void {
+        //console.log("Filtering Covers...");
+        
+        if( this.songArray.length > 0 ){
+            this.songArray.forEach( song => {
+                if( song.cover ) {
+                    song.hidden = !song.hidden;
                 }
             })
         }
