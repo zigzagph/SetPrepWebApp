09f37000050bf51a078f96be841499d14977f42a
Patrick Hutchinson <zigzagph@gmail.com>
2018-10-18 02:02:30 -0500

    added locale api

--- server/routes.js
+++ server/routes.js
@@ -66,13 +66,25 @@ router.get('/locales', function(req, res, next) {
     });
 });
 
-// Returns the previous 3 shows
+// returns a number of shows for a given city/locale
+router.get('/locale', function(req, res, next) {
+    options.url = ptURL + '/api/bands/' + bandId + '/setlists/paged?page=1&pageSize=' + req.query.shows + '&timespan=past&locale=' + req.query.locale;
+    request(options, (err, response, html) => {
+        if (err) {console.log(`Error : ${err}`); }
+        if (html) { 
+            return res.send(JSON.parse(html));
+        }
+        //if (response) { return res.send(response); }
+    });
+});
+
+// Returns a number of previous shows for the given value
 router.get('/previous', function(req, res, next) {
     options.url = ptURL + '/api/bands/' + bandId + '/setlists/paged?page=1&pageSize=' + req.query.shows + '&timespan=past';
     request(options, (err, response, html) => {
         if (err) {console.log(`Error : ${err}`); }
         if (html) { 
-            return res.send(JSON.parse(html)); 
+            return res.send(JSON.parse(html));
         }
         //if (response) { return res.send(response); }
     });
